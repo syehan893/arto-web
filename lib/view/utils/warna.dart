accede0bf36ff8849bc71190b541412252ca2740
randyapr12 <randyaoril29@gmail.com>
2023-04-02 21:04:38 +0700

    fixs bugs

--- lib/view/utils/warna.dart
+++ lib/view/utils/warna.dart
@@ -1,1 +1,6 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFEA5455);
+const secunderColor = Color(0xffffffff);
+const loginButtonColor = Color(0xFF002B5B);
+const columnLoginColor = Color(0xffD9D9D9);
