10259eaf053db4845809578712d7fa208e5ab99d
randyapr12 <randyaoril29@gmail.com>
2023-05-18 17:43:14 +0700

    help done

--- lib/view/screens/help/help.dart
+++ lib/view/screens/help/help.dart
@@ -22,147 +22,170 @@ class Help extends StatelessWidget {
             ),
           ),
           Container(
-              width: lebarBody * 0.60,
-              height: tinggiBody,
-              color: Colors.white12,
-              child: Column(children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 48, top: 31),
-                    child: Container(
-                      child: Text(
-                        "Help Center",
-                        style: TextStyle(fontSize: 32),
-                      ),
-                    ), //judul
-                  ),
-                ),
-                SizedBox(
-                  height: 52,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 48, right: 19),
+            width: lebarBody * 0.60,
+            height: tinggiBody,
+            color: Colors.white12,
+            child: Column(children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 48, top: 31),
                   child: Container(
-                    height: 1,
-                    width: lebarBody,
-                    color: Colors.black,
-                  ), //garis
+                    child: Text(
+                      "Help Center",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                  ), //judul
                 ),
-                SizedBox(
-                  height: 38,
-                ),
-                Container(
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                          padding: EdgeInsets.only(left: 48),
-                          child: Text(
-                            "Hi, Randy",
-                            style: TextStyle(fontSize: 32),
-                          ),
+              ),
+              SizedBox(
+                height: 52,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 48, right: 19),
+                child: Container(
+                  height: 1,
+                  width: lebarBody,
+                  color: Colors.black,
+                ), //garis
+              ),
+              SizedBox(
+                height: 38,
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 48),
+                        child: Text(
+                          "Hi, Randy",
+                          style: TextStyle(fontSize: 32),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
-                Container(
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                          padding: EdgeInsets.only(left: 52),
-                          child: Text(
-                            "Need a Help?",
-                            style: TextStyle(fontSize: 20),
-                          ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 52),
+                        child: Text(
+                          "Need a Help?",
+                          style: TextStyle(fontSize: 20),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
-                SizedBox(
-                  height: 38,
+              ),
+              SizedBox(
+                height: 38,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 52, right: 19),
+                height: 29,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: Color.fromARGB(255, 7, 9, 121),
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 52, right: 19),
-                  height: 29,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7),
-                    color: Color.fromARGB(255, 7, 9, 121),
-                  ),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 11,
-                      ),
-                      Text(
-                        'Kontak Kami',
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ],
-                  ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 11,
+                    ),
+                    Text(
+                      'Kontak Kami',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 14,
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 52,
+                    ),
+                    helpInfoCard(
+                        image: 'web/assets/online_support.png',
+                        name: 'Call Center 1',
+                        telp: '0251 1234 5670'),
+                    SizedBox(
+                      width: 22,
+                    ),
+                    helpInfoCard(
+                        image: 'web/assets/logo_wa.png',
+                        name: 'Official Whatsapp',
+                        telp: '0251 1234 5670'),
+                    SizedBox(
+                      width: 19,
+                    ),
+                  ],
                 ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: 52,
-                      ),
-                      helpInfoCard(
-                          image: 'web/assets/online_support.png',
-                          name: 'Call Center 1',
-                          telp: '0251 1234 5670'),
-                      SizedBox(
-                        width: 22,
-                      ),
-                      helpInfoCard(
-                          image: 'web/assets/logo_wa.png',
-                          name: 'Official Whatsapp',
-                          telp: '0251 1234 5670'),
-                      SizedBox(
-                        width: 19,
-                      ),
-                    ],
-                  ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 52,
+                    ),
+                    helpInfoCard(
+                        image: 'web/assets/online_support.png',
+                        name: 'Call Center 2',
+                        telp: '0251 1234 5670'),
+                    SizedBox(
+                      width: 22,
+                    ),
+                    helpInfoCard(
+                        image: 'web/assets/logo_telegram.png',
+                        name: 'Official Telegram',
+                        telp: '0251 1234 5670'),
+                    SizedBox(
+                      width: 19,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 8,
+              ),
+              SizedBox(
+                height: 34,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 52, right: 19),
+                height: 29,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: Color.fromARGB(255, 7, 9, 121),
                 ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: 52,
-                      ),
-                      helpInfoCard(
-                          image: 'web/assets/online_support.png',
-                          name: 'Call Center 2',
-                          telp: '0251 1234 5670'),
-                      SizedBox(
-                        width: 22,
-                      ),
-                      helpInfoCard(
-                          image: 'web/assets/logo_telegram.png',
-                          name: 'Official Telegram',
-                          telp: '0251 1234 5670'),
-                      SizedBox(
-                        width: 19,
-                      ),
-                    ],
-                  ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 11,
+                    ),
+                    Text(
+                      'Office Location',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
                 ),
-              ])),
-          SizedBox(
-            height: 8,
+              ),
+              SizedBox(
+                height: 14,
+              ),
+            ]),
           ),
           Expanded(
               child: Container(
