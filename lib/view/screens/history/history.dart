a4bb45e591800fd78c140751501c2ce18654a68b
unknown <P020523002@bussan.co.id>
2023-05-18 18:27:53 +0700

    bikin history page

---
+++ lib/view/screens/history/history.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../../utils/warna.dart';
+
+class History extends StatefulWidget {
+  const History({super.key});
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  String? selectedNominalTransfer;
+
+  @override
+  Widget build(BuildContext context) {
+    double tinggiBody = MediaQuery.of(context).size.height;
+    double lebarBody = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        body: Row(
+      children: [
+        Expanded(
+          child: Container(
+            width: lebarBody * 0.20,
+            height: tinggiBody,
+            color: sideLeftColor
+          ),
+        ), //kiri
+        Container(
+          width: lebarBody * 0.60,
+          height: tinggiBody,
+          color: Colors.white54,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Container(
+                    child: Text(
+                      "history",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                  ), //judul
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 2,
+                  width: lebarBody,
+                  color: Colors.black,
+                ), //garis
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Text("your balance"),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Text(
+                          "Rp.1.000.000,-",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: lebarBody * 0.01,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ), //tengah
+        Expanded(
+          child: Container(
+            width: lebarBody * 0.20,
+            height: tinggiBody,
+            color: sideRightColor,
+          ),
+        ), //kanan
+      ],
+    ));
+  }
+}
