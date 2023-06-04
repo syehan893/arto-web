3dd8a808f02df835cefcc63c2a4abed65211659d
unknown <P020523002@bussan.co.id>
2023-06-04 20:36:56 +0700

    layout user

--- lib/view/screens/user/user.dart
+++ lib/view/screens/user/user.dart
@@ -19,36 +19,39 @@ class _UserState extends State<User> {
       child: Container(
         width: lebarBody * 0.20,
         height: tinggiBody,
-        color: Colors.redAccent,
+        color: sideRightColor,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 20, top: 27, right: 10),
+              padding: const EdgeInsets.only(left: 69, top: 27, right: 54),
               child: person(
                 userName: "Aditya Abdi",
                 email: "adityaabdi01@gmail.com",
               ),
             ), // user
             SizedBox(
-              height: 30,
+              height: 43,
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(bottom: 14,left: 29),
               child: Text(
                 "Virtual Card",
                 style: TextStyle(color: Colors.white, fontSize: 24),
               ),
             ),
-            VRcard(), // virtual card
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(left: 26,right: 25),
+              child: VRcard(),
+            ), // virtual card
+            Padding(
+              padding: const EdgeInsets.only(left: 29,right: 25),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
                     height: 20,
-                    width: 160,
+                    width: 180,
                     child: Center(
                         child: Text(
                       "1232 4567 8927",
@@ -59,9 +62,10 @@ class _UserState extends State<User> {
                       color: Colors.white,
                     ),
                   ), // no req
+                  SizedBox(width: 40,),
                   Container(
                     height: 20,
-                    width: 60,
+                    width: 70,
                     child: Center(
                         child: Text(
                       "COPY",
@@ -76,19 +80,19 @@ class _UserState extends State<User> {
               ),
             ), // onmer reg
             SizedBox(
-              height: 10,
+              height: 49,
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(left: 26,right:25 ),
               child: Text(
                 "Promotion",
                 style: TextStyle(fontSize: 24, color: Colors.white),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(left: 26,right: 25),
               child: Container(
-                height: 150,
+                height: 211,
                 width: 292,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(11),
@@ -165,56 +169,53 @@ class VRcard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 10, left: 10),
-      child: Card(
-        color: loginButtonColor,
-        //color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(14),
-        ),
-        child: Container(
-          height: 171,
-          width: 294,
-          //child: Image.asset('web/assets/logo-arto.png'),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 30,
-                  width: 50,
-                  child: Image.asset('web/assets/visa.png'),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  "Rp.2.000.000,-",
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 1,
-                  width: 100,
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "ADITYA",
-                  style: TextStyle(color: Colors.white),
-                ),
-                Text(
-                  "ABDI",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
+    return Card(
+      color: loginButtonColor,
+      //color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Container(
+        height: 171,
+        width: 294,
+        //child: Image.asset('web/assets/logo-arto.png'),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 30,
+                width: 50,
+                child: Image.asset('web/assets/visa.png'),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Rp.2.000.000,-",
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 1,
+                width: 100,
+                color: Colors.white,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "ADITYA",
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                "ABDI",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
           ),
         ),
       ),
